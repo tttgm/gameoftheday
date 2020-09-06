334129a6c47aceb61b2256118f54296548b64130
Tom M <tmckenzie.nz@gmail.com>
2020-09-06 13:06:30 +1000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './assets/logo1.svg';
+import chevrons from './assets/chevrons-down.svg';
 import './App.css';
+
+function SectionHeader(props) {
+  return (
+    <>
+      <img src={chevrons} />
+      <h2>{props.title}</h2>
+    </>
+  )
+}
+
+function SectionSubHeader(props) {
+  return (
+    <h4>{props.subtitle}</h4>
+  )
+}
+
+function SingleDate() {
+  return (
+    <>
+      <p>Mon</p>
+      <p>19</p>
+    </>
+  )
+}
+
+function DatePicker() {
+  return (
+    <div className="date-picker">
+      <h3>January</h3>
+      <SingleDate />
+      <SingleDate />
+      <SingleDate />
+      <SingleDate />
+    </div>
+  )
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <img src={logo} />
+      <p>your spoiler free guide to on-demand sports</p>
+      <DatePicker />
+      <SectionHeader title="NBA" />
+      <SectionSubHeader subtitle="Tier 1 - Must Watch" />
+      <div></div>
+      <SectionSubHeader subtitle="Tier 2 - Worth a Watch" />
     </div>
   );
 }
